ff2ad52aa13d3574e506e62cd85b576bdff1ad61
pengyao <1757254275@qq.com>
2018-07-03 22:20:21 +0800

    spring session

---
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,83 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServiceResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author : pengyao
+ * @Date: 2018/6/2 20: 36
+ */
+@Controller
+@RequestMapping("/user/springsession/")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService iUserService;//与service中注解里的名称一致
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.GET)
+    @ResponseBody //将返回结果自动序列化为json
+    public ServiceResponse<User> login(String username, String password, HttpSession session,HttpServletResponse httpServletResponse){
+        //调用service,servce调用mybatis的dao层
+        ServiceResponse<User> response = iUserService.login(username,password);
+        if (response.isSuccess()){
+
+
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+            //CookieUtil.writeLoginToken(httpServletresponse,session.getId());
+            //将session保存到redis
+            //RedisShardedPoolUtil.setEx(session.getId(),Const.RedisCacheExtime.REDIS_SESSION_EXTIME, JsonUtil.obj2String(response.getData()));
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+    @ResponseBody //将返回结果自动序列化为json
+    public ServiceResponse<User> logout(HttpSession session,HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse){
+        session.removeAttribute(Const.CURRENT_USER);
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        CookieUtil.delLoginToken(httpServletRequest,httpServletResponse);
+//        RedisShardedPoolUtil.del(loginToken);
+        return ServiceResponse.createBySuccess();
+    }
+
+
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceResponse<User> getUserInfo(HttpSession session,HttpServletRequest httpServletRequest){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        if (StringUtils.isEmpty(loginToken)){
+//            return ServiceResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
+//        }
+//        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+//        User user = JsonUtil.string2Obj(userJsonStr,User.class);
+        if (user != null){
+            return ServiceResponse.createBySuccess(user);
+        }
+        return ServiceResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
+    }
+
+
+}
